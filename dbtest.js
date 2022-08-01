e85c11692ad645a330757d539fed8b7fb80d9752
Tfg20 <tgigliotti2020@fau.edu>
2022-07-31 22:02:32 -0400

    Backend
    Backend Full

---
+++ dbtest.js
@@ -0,0 +1,44 @@
+const { MongoClient, ObjectId } = require("mongodb");
+const Express = require("express");
+const BodyParser = require('body-parser');
+var uri = "mongodb+srv://Test:Password@simulationresults.uariljv.mongodb.net/?retryWrites=true&w=majority"; // This is set to a user to have temporary read/write on my account. Will need to be changed with new MongoDB account.
+const server = Express();
+const client = new MongoClient(uri);
+const cors = require('cors');
+server.use(cors());
+server.use(BodyParser.json());
+server.use(BodyParser.urlencoded({ extended: true }));
+
+
+
+var collection;
+
+
+server.post("/Results", async (req, response, next) => {
+    try {
+        let result = await collection.insertOne(req.body);
+        response.send(result);
+    } catch (e) {
+        response.status(500).send({ message: e.message });
+    }
+});
+
+server.get("/Results/:record_id", async (req, response, next) => {
+    var record_id = req.params.record_id;
+    try {
+        let result = await collection.findOne({"Username":record_id});
+        response.send(result);
+    } catch (e) {
+        response.status(500).send({ message: e.message });
+    }
+});
+
+server.listen("3000", async () => {
+    try {
+        await client.connect();
+        collection = client.db("Simulation").collection("Results");
+        console.log("Listening at :3000...");
+    } catch (e) {
+        console.error(e);
+    }
+});
